fe1b425531a86b9d562dd7783afdb092a0492622
shubham <shubhamkumarhalwa@gmail.com>
2023-06-23 16:01:23 +0530

    code

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('working fine on Staging')
+
+  res.send('+++++++++++++++++++++++++++++++++++! Backend API MICROSERVICE working testing !+++++++++++++++++++++++++++++++++++++++++++++++++++++');
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
