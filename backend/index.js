3b270905f065da981c13d1bfd012035e141eb159
shubham <shubhamkumarhalwa@gmail.com>
2023-06-26 01:41:46 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
   console.log('working fine on Staging')
 
-  res.send('+++++++++++++++++++++++++++++++++++! Backend API MICROSERVICE working testing for dev!+++++++++++++++++++++++++++++++++++++++++++++++++++++');
+  res.send('+++++++++++++++++++++++++++++++++++! Backend API MICROSERVICE working testing for dev env!+++++++++++++++++++++++++++++++++++++++++++++++++++++');
 });
 
 const port = process.env.PORT || 4000;
